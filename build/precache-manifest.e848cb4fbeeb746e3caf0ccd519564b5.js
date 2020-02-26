24d494c2dfb34bfe406a4cd9b08406cff53fdd34
gleyconxavier <gleyconxcarlos@gmail.com>
2020-02-26 19:34:47 -0400

    Updates

---
+++ build/precache-manifest.e848cb4fbeeb746e3caf0ccd519564b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deadc1bf50e960087abc060f7305cb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1c664bbcee3ec439f0",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43317829ac63ebc7d0ab",
+    "url": "/static/css/main.f0a591ae.chunk.css"
+  },
+  {
+    "revision": "2e1c664bbcee3ec439f0",
+    "url": "/static/js/2.2a69eaa7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2a69eaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43317829ac63ebc7d0ab",
+    "url": "/static/js/main.64b6e06b.chunk.js"
+  },
+  {
+    "revision": "29247c02f79741d4d17e",
+    "url": "/static/js/runtime-main.44525653.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
